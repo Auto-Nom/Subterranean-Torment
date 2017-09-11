eb5a4a3f724e7e5214e768f02521f80677afbe18
auto_nom <blayde777@gmail.com>
2017-09-11 23:38:33 +0200

    begin stat overhaul

--- components/fighter.py
+++ components/fighter.py
@@ -2,17 +2,79 @@
 Fighter component for Subterranean Torment
 """
 
+from random import randint
+
 import libtcodpy as libtcod
 
 from game_messages import Message
 
 class Fighter:
-    def __init__(self, hp, defense, power, xp=0):
-        self.base_max_hp = hp
-        self.hp = hp
-        self.base_defense = defense
-        self.base_power = power
+    def __init__(self, strength=10, agility=10, constitution=10, intelligence=10, cunning=10, base_str_acc=50, base_agi_acc=50, base_dodge=5, base_hp=100, base_hp_regen=1, base_spellpower=50, base_magic_res=5, base_crit_chance=5, base_insane_resist=5, base_phys_res=5, base_damage=5, xp=0):
+        self.base_strength = strength
+        self.base_agility = agility
+        self.base_constitution = constitution
+        self.base_intelligence = intelligence
+        self.base_cunning = cunnning
+
+        self.base_str_acc = base_str_acc
+        self.base_agi_acc = base_agi_acc
+        self.base_dodge = base_dodge
+        self.base_max_hp = base_hp
+        self.base_hp_regen = base_hp_regen
+        self.base_spellpower = base_spellpower
+        self.base_magic_res = base_magic_res
+        self.base_crit_chance = base_crit_chance
+        self.base_insane_res = base_insane_res
+        self.base_phys_res = base_phys_res
+        self.base_damage = base_damage
         self.xp = xp
+
+        self.hp = self.max_hp
+
+    @property
+    def strength(self):
+        if self.owner and self.owner.equipment:
+            bonus = self.owner.equipment.strength_bonus
+        else:
+            bonus = 0
+
+        return self.base_strength + bonus
+
+    @property
+    def agility(self):
+        if self.owner and self.owner.equipment:
+            bonus = self.owner.equipment.agility_bonus
+        else:
+            bonus = 0
+
+        return self.base_agility + bonus
+
+    @property
+    def constitution(self):
+        if self.owner and self.owner.equipment:
+            bonus = self.owner.equipment.constitution_bonus
+        else:
+            bonus = 0
+
+        return self.base_constitution + bonus
+
+    @property
+    def intelligence(self):
+        if self.owner and self.owner.equipment:
+            bonus = self.owner.equipment.intelligence_bonus
+        else:
+            bonus = 0
+
+        return self.base_intelligence + bonus
+
+    @property
+    def cunning(self):
+        if self.owner and self.owner.equipment:
+            bonus = self.owner.equipment.cunning_bonus
+        else:
+            bonus = 0
+
+        return self.base_cunnning + bonus
 
     @property
     def max_hp(self):
@@ -21,29 +83,131 @@ class Fighter:
         else:
             bonus = 0
 
-        return self.base_max_hp + bonus
+        stat_mod = (self.constitution - 10) * 5
+
+        return self.base_max_hp + stat_mod + bonus
 
     @property
-    def power(self):
+    def strength_accuracy(self):
         if self.owner and self.owner.equipment:
-            bonus = self.owner.equipment.power_bonus
+            bonus = self.owner.equipment.str_acc_bonus
         else:
             bonus = 0
 
-        return self.base_power + bonus
+        stat_mod = (self.strength - 10) * 4
+
+        return self.base_str_acc + stat_mod + bonus
 
     @property
-    def defense(self):
+    def agility_accuracy(self):
         if self.owner and self.owner.equipment:
-            bonus = self.owner.equipment.defense_bonus
+            bonus = self.owner.equipment.agi_acc_bonus
         else:
             bonus = 0
 
-        return self.base_defense + bonus
+        stat_mod = (self.agility - 10) * 4
 
-    def take_damage(self, amount):
+        return self.base_agi_acc + stat_mod + bonus
+
+    @property
+    def dodge(self):
+        if self.owner and self.owner.equipment:
+            bonus = self.owner.equipment.dodge_bonus
+        else:
+            bonus = 0
+
+        stat_mod = (self.agility - 10) * 4
+
+        return self.base_dodge + stat_mod + bonus
+
+    @property
+    def hp_regen(self):
+        if self.owner and self.owner.equipment:
+            bonus = self.owner.equipment.hp_regen_bonus
+        else:
+            bonus = 0
+
+        stat_mod = (self.constitution - 10) // 2
+        if stat_mod < 0:
+            stat_mod = 0
+
+        return self.base_hp_regen + stat_mod + bonus
+
+    @property
+    def spellpower(self):
+        if self.owner and self.owner.equipment:
+            bonus = self.owner.equipment.spellpower_bonus
+        else:
+            bonus = 0
+
+        stat_mod = (self.intelligence - 10) * 10
+
+        total = self.base_stat + stat_mod + bonus
+        if total < 0:
+            total = 0
+
+        return total
+
+    @property
+    def magic_resist(self):
+        if self.owner and self.owner.equipment:
+            bonus = self.owner.equipment.magic_res_bonus
+        else:
+            bonus = 0
+
+        stat_mod = (self.intelligence - 10) * 4
+
+        return self.base_magic_res + stat_mod + bonus
+
+    @property
+    def crit_chance(self):
+        if self.owner and self.owner.equipment:
+            bonus = self.owner.equipment.crit_chance_bonus
+        else:
+            bonus = 0
+
+        stat_mod = (self.cunning - 10) * 4
+
+        return self.crit_chance + stat_mod + bonus
+
+    @property
+    def insanity_resist(self):
+        if self.owner and self.owner.equipment:
+            bonus = self.owner.equipment.insane_res_bonus
+        else:
+            bonus = 0
+
+        stat_mod = (self.cunning - 10) * 4
+
+        return self.base_insane_res + stat_mod + bonus
+
+    @property
+    def physical_resist(self):
+        if self.owner and self.owner.equipment:
+            bonus = self.owner.equipment.phys_res_bonus
+        else:
+            bonus = 0
+
+        return self.base_phys_res + bonus
+
+    @property
+    def damage(self):
+        if self.owner and self.owner.equipment:
+            bonus = self.owner.equipment.damage_bonus
+        else:
+            bonus = 0
+
+        return self.base_magic_res + bonus
+
+    def take_damage(self, amount, physical=True, magical=False):
         results = []
         
+        if physical:
+            amount *= (1- (self.physical_resist / 100))
+        
+        if magical:
+            amount *= (1 - (self.magical_resist / 100))
+
         self.hp -= amount
         
         if self.hp <= 0:
@@ -57,16 +221,33 @@ class Fighter:
         if self.hp >= self.max_hp:
             self.hp = self.max_hp
 
-    def attack(self, target):
+    def attack(self, target, accuracy_stat):
         results = []
 
-        damage = self.power - target.fighter.defense
+        dmg = self.damage
+        crit = False
+        
+        hit = randint(1, 100)
+        if hit < self.crit_chance:
+            crit = True
 
-        if damage > 0:
-            results.append({'message': Message('{0} attacks {1} for {2} hit points.'.format(self.owner.name.capitalize(), target.name, str(damage)), libtcod.white)})
-            results.extend(target.fighter.take_damage(damage))
-        else:
-            results.append({'message':Message('{0} attacks {1} but does no damage.'.format(self.owner.name.capitalize(), target.name), libtcod.white)})
+        if accuracy_stat == "strength":
+            hit *= self.strength_accuracy
+        
+        elif accuracy_stat == "agility":
+            hit *= self.agility_accuracy
+
+        enemy_dodge = target.fighter.dodge * randint(1, 100)
+
+        if crit or (hit > enemy_dodge):
+
+            if crit:
+                dmg *= 2
+                results.append({'message': Message('{0} deals a critical hit on {1} for {2} hit points!'.format(self.owner.name.capitalize(), target.name, str(dmg)), libtcod.red)})
+                results.extend(target.fighter.take_damage(dmg))
+            else:
+                results.append({'message': Message('{0} attacks {1} for {2} hit points.'.format(self.owner.name.capitalize(), target.name, str(dmg)), libtcod.white)})
+                results.extend(target.fighter.take_damage(dmg))
 
         return results
 
